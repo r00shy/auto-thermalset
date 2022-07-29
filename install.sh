d51679b7ef66ce9360871be82638c20295521eaf
PaulLm <104359913+r00shy@users.noreply.github.com>
2022-07-29 16:27:45 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ if [ "$1" = "-rebooted" ]; then
   {
     echo "Installing part 2/2."
     echo "Downloading script"
-    curl -H 'Authorization: token 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' -H 'Accept: application/vnd.github.v3.raw' -o /etc/init/fanctrl-custom.conf "https://raw.githubusercontent.com/r00shy/auto-thermalset/main/customFanTemps.config"
+    curl -H 'Authorization: token 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' -H 'Accept: application/vnd.github.v3.raw' -o /etc/init/fanctrl-custom.conf -L "https://raw.githubusercontent.com/r00shy/auto-thermalset/main/customFanTemps.config"
     echo "Starting service"
     initctl start customFanTemps
     initctl reload customFanTemps
